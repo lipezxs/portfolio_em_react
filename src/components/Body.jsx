d33b2ecdfe8f30f5c2d7e774fee1de83eb492325
lipezxs <fa8561141@gmail.com>
2024-09-11 17:26:13 -0300

    atualizando

---
+++ src/components/Body.jsx
@@ -0,0 +1,13 @@
+
+const Body = () => {
+    
+  return (
+    <div className="main">
+        <div className="principal">
+            teste 2
+        </div>
+    </div>
+  )
+}
+
+export default Body
---
+++ src/components/Cabecalho.jsx
@@ -0,0 +1,28 @@
+
+const Cabecalho = () => {
+  return (
+  
+    <div className="navbar">
+        <ul>
+          <li>Felipe Alves</li>
+        </ul>
+        <ul className="interativos">
+          <li>
+            <a href="#">Sobre mim</a>
+          </li>
+          <li>
+            <a href="#">Conhecimentos</a>
+          </li>
+          <li>
+            <a href="#">Projetos</a>
+          </li>
+          <li>
+            <a href="#">Fale comigo!</a>
+          </li>
+          <ul/>
+        </ul>
+    </div>
+  )
+}
+
+export default Cabecalho
